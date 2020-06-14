129b9b9e938ca4cb8221ba90eae977d0130facea
Zenghuqiang <254910920@qq.com>
2020-06-14 12:34:49 +0800

    解决git冲突

--- csiShop/src/main/java/com/chinasoft/service/impl/ProductCollectionServiceImpl.java
+++ csiShop/src/main/java/com/chinasoft/service/impl/ProductCollectionServiceImpl.java
@@ -1,19 +1,13 @@
 package com.chinasoft.service.impl;
 
-<<<<<<< HEAD
+
 import com.chinasoft.service.ProductCollectionService;
 
-public class ProductCollectionServiceImpl implements ProductCollectionService{
-=======
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
->>>>>>> 4a6d0c718c9d0e745b1e4160408bc78247ccad38
-
 import com.chinasoft.dao.ProductCollectionDao;
 import com.chinasoft.entity.ProductCollection;
-import com.chinasoft.service.ProductCollectionService;
 
 @Service
 public class ProductCollectionServiceImpl implements ProductCollectionService{
