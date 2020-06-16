a7a685a63afff12303f602a0f44b27e22c2126c7
Light-Yu <66463048+Light-Yu@users.noreply.github.com>
2020-06-16 09:46:33 +0800

    Update UserService.java

--- csiShop/src/main/java/com/chinasoft/service/UserService.java
+++ csiShop/src/main/java/com/chinasoft/service/UserService.java
@@ -1,5 +1,12 @@
 package com.chinasoft.service;
+ 
+import java.util.List;
+
+import com.chinasoft.entity.*;;
 
 public interface UserService {
-
+    //登录
+	public User login(String name);
+	
+	
 }
